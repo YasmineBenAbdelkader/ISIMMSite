70cd88847adb948b6bf4d3745a71e90c8748f2b0
yasmine <benabdelkaderyasmine2@gmail.com>
2024-01-25 23:41:26 +0100

    connect_DataBase

--- Back/Config/database.js
+++ Back/Config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-const url = "mongodb+srv://mestirimouna2:mounamestiri123@cluster0.xbpxrbq.mongodb.net/test?retryWrites=false"
+const url = "mongodb+srv://mestirimouna2:4MOQJbguIcYHVjez@cluster0.xbpxrbq.mongodb.net/test?retryWrites=false"
 
 async function connect() {
     try {
