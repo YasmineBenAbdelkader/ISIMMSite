02ff8c985218c0e12a9ec6a46638fb2ff250e48d
Mouna <mestirimouna2@gmail.com>
2024-01-25 14:25:17 +0100

    database

--- Back/Config/database.js
+++ Back/Config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-const url = ""
+const url = "mongodb+srv://mestirimouna2:<password>@cluster0.xbpxrbq.mongodb.net/?retryWrites=true&w=majority"
 
 async function connect() {
     try {
