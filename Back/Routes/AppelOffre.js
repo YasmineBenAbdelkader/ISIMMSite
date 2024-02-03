a9fd1f615fe7b2d0babceae163cd4a2a49c2fcbc
Mouna <mestirimouna2@gmail.com>
2024-02-03 21:04:54 +0100

    formulaire_backend

--- Back/Routes/AppelOffre.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { customParticipationMethod } = require('../Controllers/AppelOffre');
-const uploadFile = require('../middlewares/uploadFile');
-
-router.post('/participate-offre/:PubAppelOffre', uploadFile.single('Fichier'), customParticipationMethod);
-
-module.exports = router;
